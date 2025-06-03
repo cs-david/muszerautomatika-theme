dcac32d639eb433b6f696c51da94c41d323c176d
David Csere <csere@Mac.home>
2025-06-03 17:03:41 +0200

    Self hosted Fontawesome

--- functions.php
+++ functions.php
@@ -123,7 +123,8 @@ function muszerautomatika_theme_scripts() {
 	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap', false);
     wp_enqueue_style('custom-styles', get_template_directory_uri() . '/css/styles.css', array(), null);
     wp_enqueue_style( 'owl-carousel-css', get_template_directory_uri() . '/css/owl-carousel/owl.carousel.css', array(), true );
-    wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/33aef8f39e.js', array(), null, true);
+    wp_enqueue_script('font-awesome', get_template_directory_uri() . '/css/fontawesome/css/fontawesome.min.css', array(), null);
+    wp_enqueue_script('font-awesome-brands', get_template_directory_uri() . '/css/fontawesome/css/brands.min.css', array(), null);
     wp_enqueue_script( 'owl-carousel', get_template_directory_uri() . '/js/owl-carousel/owl.carousel.js', array('jquery'), _S_VERSION, true );
 	wp_enqueue_script( 'muszerautomatika-theme-scripts', get_template_directory_uri() . '/js/scripts.js', array(), _S_VERSION, true );
 
